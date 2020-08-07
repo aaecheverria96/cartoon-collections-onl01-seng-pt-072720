8bf95c6f76170af423a6067f9eabe4bc14385194
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-07 22:08:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,9 @@ def long_planeteer_calls (array)
 end
 
 def find_the_cheese (cheese_types)
+  first_cheese = []
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.find do |cheese|
+  first_cheese << cheese_types.find do |cheese| 
   end
+  first_cheese
 end
