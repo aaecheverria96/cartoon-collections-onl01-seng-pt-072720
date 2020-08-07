52a389d6cd3752168d13888ae521bc7d8cef956d
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-07 22:26:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def long_planeteer_calls (array)
 end
 
 def find_the_cheese (array)
-  array.find do |cheese| 
+  array.find do |cheese|
   cheese_types = ["cheddar", "gouda", "camembert"]
   end
 end
